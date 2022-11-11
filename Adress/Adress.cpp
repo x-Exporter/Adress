e74c1c9aa68ca0b8dad614fe28ca61e3874400c0
Oleg Shchedrin <koboltkob@gmail.com>
2022-11-11 23:52:01 +0300

    Добавьте файлы проекта.

---
+++ Adress/Adress.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Adress
+{
+private:
+	string city;
+	string street;
+	int house_number;
+	int apartment_number;
+};
+
+int main()
+{
+
+}
